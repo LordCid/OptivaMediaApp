85e5cd67175a88ddfcdf9d989a98503e1b076e80
Cid, Albert <Albert.Cid@gft.com>
2022-04-19 13:03:56 +0200

    import optimization

--- app/src/main/java/com/example/omapp/presentation/MovieListFragment.kt
+++ app/src/main/java/com/example/omapp/presentation/MovieListFragment.kt
@@ -5,11 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DiffUtil
-import com.example.omapp.R
 import com.example.omapp.databinding.FragmentMovieListBinding
-import com.example.omapp.domain.model.Movie
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
