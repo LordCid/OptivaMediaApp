536da18cc238f633fb5b8c4d5afb46252a66f973
Cid, Albert <Albert.Cid@gft.com>
2022-04-25 15:42:20 +0200

    code style

--- app/src/main/java/com/example/omapp/common/Extensions.kt
+++ app/src/main/java/com/example/omapp/common/Extensions.kt
@@ -7,10 +7,8 @@ import java.util.*
 
 const val FORMAT_DATE_LONG_TIME = "HH:mm"
 
-
 fun Long.formatDuration() : String {
     return try {
         SimpleDateFormat(FORMAT_DATE_LONG_TIME, Locale.getDefault()).format(Date(this))
     } catch (e: ParseException) { EMPTY_STRING }
 }
-
